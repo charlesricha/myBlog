2a760246cc7c7b37d1b49be63883971b04bca49d
Charles Muthui <126247035+charlesricha@users.noreply.github.com>
2025-08-26 17:49:49 +0300

    Implemented next_public due to errors in vercel
    - Changed the "firebase.ts" values in config to allow access in public due to some errors in hosting the app on vercel

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,24 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   projectId: "pixel-chronicles-1f1ol",
   appId: "1:755162815313:web:964eb093856040e1461170",
   storageBucket: "pixel-chronicles-1f1ol.appspot.com",
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "pixel-chronicles-1f1ol.firebaseapp.com",
   messagingSenderId: "755162815313",
+}; */
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
